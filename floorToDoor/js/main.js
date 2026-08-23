ce478ce4ae261b3d4c929dfe1a898296174822dc
Ethan <u25517806.tuks.ac.za>
2026-08-23 15:27:58 +0200

    deleted old files from github old file preservation mechanism

--- floorToDoor/js/main.js
+++
@@ -1,9 +0,0 @@
-//code used by EVERY page in here
-
-function toggleMobileNav() {
-  const navLinks = document.getElementById('navLinks');
-
-  if (navLinks) {
-    navLinks.classList.toggle('open');
-  }
-}
